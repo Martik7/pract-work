c539ffd8a741e64eabbb4c95696ac553c75f57b1
СадвокасовО <@205_4>
2018-11-22 21:52:19 +0600

    Добавьте файлы проекта.

---
+++ Project6/Source.cpp
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <locale.h>
+
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+
+
+
+
+
+	/*float y, x, k, t = 4.1, p = 3;
+
+	k = sqrt(p*t);
+
+	x = (p * pow(t, 2)) + (sqrt(k));
+
+	y = pow(tan(pow(x, 2)), 3)+ (k * t);
+
+	printf("%f\n", y);*/
+
+
+
+	/*float x = 2.7, y, t = -6, a, b;
+
+	b = sqrt(pow(x, 2) + pow(t, 2));
+
+	a = log(x);
+
+	y = pow(sqrt(abs(a - b * x)), 2);
+	printf("%f\n", y);*/
+
+	/*float x = 2.1, p = 1, y, a, b;
+
+	b = (sin(pow(p, 2)) + pow(x, 3));
+
+	a = exp(sqrt(abs(x)));
+
+	y = pow(a, 3) / pow(p, 2);
+	printf("%f\n", y);*/
+
+
+	/*float y, a = 0, b = 4, x = 1.3;
+
+	a = log(abs(x));
+
+	b = exp(2 * x) + a * x;
+	
+	y = (x * pow(a, 3)) + (pow(b, 2));
+	printf("%f\n", y);*/
+
+
+	/*float y, x = 3.5, b = 0.4,c ,a ,e;
+
+	a = log10(x);
+
+	c = pow(a, 2) + sqrt(b*x);
+
+	y = exp(2 * x) + pow(9.7, c);
+
+	printf("%f\n", y);*/
+
+
+	/*float a, b, x;
+
+	printf("¬ведите число a= ");
+	scanf_s("%f", &a);
+
+	printf("¬ведите число b= ");
+	scanf_s("%f", &b);
+
+	x = sqrt((float)a*(float)b);
+	printf("—реднее геометрическое равн€етс€: %f\n", x);*/
+
+
+	/*int N = 180;
+	N = N / 60;
+	printf("%d\n", N);*/
+
+	/*float L = (float)5 / (float)100;
+	printf("%f\n", L);*/
+
+
+	/*int z = 1;
+
+	z = z * 8;
+	printf("%d\n", z);*/
+
+	system("pause");
+
+}
